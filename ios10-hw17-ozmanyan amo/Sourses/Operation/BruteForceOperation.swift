4f79d778c3189cb299430cda29d2f0793767833b
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-08-17 04:43:23 +0300

    Write code for BruteForce operation.

---
+++ ios10-hw17-ozmanyan amo/Sourses/Operation/BruteForceOperation.swift
@@ -0,0 +1,50 @@
+//
+//  BruteForceOperation.swift
+//  ios10-hw17-ozmanyan amo
+//
+//  Created by Amahastla on 17.08.2023.
+//
+
+import Foundation
+
+class BruteForceOperation {
+    
+    func bruteForce(passwordToUnlock: String) {
+        let allowedCharacters: [String] = String().printable.map { String($0) }
+
+        var password: String = ""
+        while password != passwordToUnlock {
+            password = generateBruteForce(password, fromArray: allowedCharacters)
+            print(password)
+            
+        }
+        print(password)
+    }
+    
+    func indexOf(character: Character, _ array: [String]) -> Int {
+        return array.firstIndex(of: String(character)) ?? 2
+    }
+
+    func characterAt(index: Int, _ array: [String]) -> Character {
+        return index < array.count ? Character(array[index])
+                                   : Character("")
+    }
+    
+    func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+        
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last ?? Character(""), array) + 1) % array.count, array))
+            
+            if indexOf(character: str.last ?? Character(""), array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last ?? Character(""))
+            }
+        }
+        
+        return str
+        
+    }
+}
